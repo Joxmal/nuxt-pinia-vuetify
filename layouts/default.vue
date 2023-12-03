37f75df1a6c477729b704ce354ac308f3e4c89ae
joxmal <joxmal123@gmail.com>
2023-12-03 18:59:55 -0400

    layout casi terminado

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,21 +2,31 @@
     <v-app>
         <v-layout>
             <v-app-bar scroll-behavior="hide elevate" prominent app>
-                <v-toolbar-title>informatica-sgi</v-toolbar-title>
+                <template v-slot:prepend>
+                    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                    <v-toolbar-title>informatica-sgi</v-toolbar-title>
+                </template>
+                <template v-slot:append>
+                    <v-btn variant="text" icon="mdi-filter"></v-btn>
 
-                <v-btn variant="text" icon="mdi-filter"></v-btn>
-                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                </template>
+                
             </v-app-bar>
             
             <v-navigation-drawer
         theme="dark"
         rail
         permanent
+        @mouseover="drawer= true"
+        @mouseleave="drawer= false"
       >
         <v-list-item
           nav
           prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"
-        ></v-list-item>
+          height="45"
+          inset
+        >
+        </v-list-item>
 
         <v-divider></v-divider>
 
@@ -24,20 +34,26 @@
           density="compact"
           nav
         >
-          <v-list-item prepend-icon="mdi-view-dashboard" value="dashboard"></v-list-item>
-
-          <v-list-item prepend-icon="mdi-forum" value="messages"></v-list-item>
+          <v-list-item prepend-icon="mdi-view-dashboard" value="Inicio"></v-list-item>
+          <v-list-item prepend-icon="mdi-forum" value="mensajes"></v-list-item>
+          <v-list-item prepend-icon="mdi-desktop-classic" value="computadoras"></v-list-item>
+          <v-list-item prepend-icon="mdi-printer-outline" value="impresoras"></v-list-item>
+          <v-list-item prepend-icon="mdi-sitemap-outline" value="sistemas"></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
-      <v-navigation-drawer permanent floating v-model="drawer">
+      <v-navigation-drawer permanent floating v-model="drawer" width="200"
+      @mouseover="drawer= true"
+      @mouseleave="drawer= false"
+      >
         <v-list>
+            <v-list-item-title height="60px" class="text-center text-h5 pt-3" inset>INICIO</v-list-item-title>
+            <v-divider/>
           <v-list-item title="Home" value="home"></v-list-item>
-
           <v-list-item title="Contacts" value="contacts"></v-list-item>
-
           <v-list-item title="Settings" value="settings"></v-list-item>
         </v-list>
+
       </v-navigation-drawer>
 
 
@@ -53,35 +69,10 @@
     </v-app>
 </template>
 
-<script>
-export default {
-    data: () => ({
-        drawer: false,
-        group: null,
-        items: [
-            {
-                title: 'Foo',
-                value: 'foo',
-            },
-            {
-                title: 'Bar',
-                value: 'bar',
-            },
-            {
-                title: 'Fizz',
-                value: 'fizz',
-            },
-            {
-                title: 'Buzz',
-                value: 'buzz',
-            },
-        ],
-    }),
+<script setup>
 
-    watch: {
-        group() {
-            this.drawer = false
-        },
-    },
-}
+
+const drawer = ref(false)
+
+
 </script>
