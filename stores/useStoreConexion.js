767639238d0cbe1478999d0782831ee3bfbafe80
joxmal <joxmal123@gmail.com>
2023-12-05 01:28:33 -0400

    variable de entorno con .env

--- stores/useStoreConexion.js
+++ stores/useStoreConexion.js
@@ -5,7 +5,7 @@ import PocketBase from 'pocketbase'
 
 export const useStoreConexion = defineStore('useStoreConexion', {
     state: () => ({
-        pb_url: 'https://oficina-sgi.pockethost.io/',
+        pb_url: useRuntimeConfig().public.POCKETBASE_URL,
         avatar:`/api/files/_pb_users_auth_/`,
         pb_Valid : '',
         errorInicio: false
