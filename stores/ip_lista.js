2d080392ea29511e461951a614e75b81a3bda6ff
joxmal <joxmal123@gmail.com>
2024-02-01 11:33:33 -0400

    listas departamento desde el front

--- stores/ip_lista.js
+++ stores/ip_lista.js
@@ -58,7 +58,8 @@ export const useIPListaStore = defineStore('useIPListaStore', {
             "VIVIENDA",
             "ZETA",
             "CHUAO",
-            "EXTERIOR"
+            "EXTERIOR",
+            "LOPNA"
         ],
     }),
 
